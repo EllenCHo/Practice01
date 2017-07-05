48a123924146f5c28546c5bb63221fb1a1c52a67
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 14:20:16 +0900

    Prac5 수정(배열 사용X)

--- src/com/javaex/practice01/Prac05.java
+++ src/com/javaex/practice01/Prac05.java
@@ -6,16 +6,16 @@ public class Prac05 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] nums = new int[5];
+		int num;
 		int max = 0;
 		System.out.println("숫자를 입력하세요");
 		
 		for(int i=0; i<5; i++) {
 			System.out.print("숫자: ");
-			nums[i] = sc.nextInt();
+			num = sc.nextInt();
 			
-			if(nums[i]>max) {
-				max = nums[i];
+			if(num>max) {
+				max = num;
 			}
 		}
 		

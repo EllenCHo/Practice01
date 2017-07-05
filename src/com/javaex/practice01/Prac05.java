5adb31a16f915697a8c6229eba4421d3627eb766
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 18:00:02 +0900

    Prac05 주석

--- src/com/javaex/practice01/Prac05.java
+++ src/com/javaex/practice01/Prac05.java
@@ -11,13 +11,13 @@ public class Prac05 {
 		System.out.println("숫자를 입력하세요");
 		System.out.print("숫자: ");
 		
-		max = sc.nextInt();
+		max = sc.nextInt();					//처음 숫자를 최대값을 저장
 		
 		for(int i=0; i<4; i++) {
 			System.out.print("숫자: ");
 			num = sc.nextInt();
-			
-			if(num>max) {
+				
+			if(num>max) {					//입력받은 숫자가 저장된 최대값보다 클 경우 최대값 갱신
 				max = num;
 			}
 		}

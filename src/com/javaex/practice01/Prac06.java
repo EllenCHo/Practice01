7127d3b9b3a60da7b340257057761f6650089326
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 17:59:21 +0900

    Prac06 주석

--- src/com/javaex/practice01/Prac06.java
+++ src/com/javaex/practice01/Prac06.java
@@ -10,17 +10,17 @@ public class Prac06 {
 		int i = sc.nextInt();
 		int result = 0;
 
-		int flag = i % 2;
+		int flag = i % 2;			//짝수인 지 홀수인 지 플래그
 
 		switch (flag) {
-		case 0:
-			for (int j = 2; j <= i; j+=2) {
+		case 0:						//짝수일 경우
+			for (int j = 2; j <= i; j+=2) {			//2부터 입력한 수까지 +2
 				result += j;
 			}
 			break;
 
-		case 1:
-			for (int j = 1; j <= i; j+=2) {
+		case 1:						//홀수일 경우
+			for (int j = 1; j <= i; j+=2) {			//1부터 입력한 수까지 +2
 				result += j;
 			}
 			break;

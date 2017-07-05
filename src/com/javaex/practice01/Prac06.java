6a493cb8d184eccec50e07703f2a6caed790acc9
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 14:22:02 +0900

    Prac06 정렬

--- src/com/javaex/practice01/Prac06.java
+++ src/com/javaex/practice01/Prac06.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 public class Prac06 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("숫자를 입력하세요");
 		int i = sc.nextInt();
-		int result=0;
-		
-		int flag = i%2;
-		
-		switch(flag) {
+		int result = 0;
+
+		int flag = i % 2;
+
+		switch (flag) {
 		case 0:
-			for(int j=1; j<=i; j++) {
-					if(j%2==0) {
-						result += j;
-					}
+			for (int j = 1; j <= i; j++) {
+				if (j % 2 == 0) {
+					result += j;
+				}
 			}
 			break;
-			
+
 		case 1:
-			for(int j=1; j<=i; j++) {
-				if(j%2==1) {
+			for (int j = 1; j <= i; j++) {
+				if (j % 2 == 1) {
 					result += j;
 				}
 			}
 			break;
 		}
-		
-		System.out.println("결과값: "+result);
+
+		System.out.println("결과값: " + result);
 		sc.close();
 	}
 

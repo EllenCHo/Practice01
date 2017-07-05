37d4648fd294859843c2b34a649cba0eb6b8656e
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 11:15:08 +0900

    Prac04_finish

--- src/com/javaex/practice01/Prac04.java
+++ src/com/javaex/practice01/Prac04.java
@@ -2,4 +2,14 @@ package com.javaex.practice01;
 
 public class Prac04 {
 
+	public static void main(String[] args) {
+		for(int i=1; i<11; i++) {
+			for(int j=i; j<10+i; j++) {
+				System.out.print(j+"\t");
+			}
+			
+			System.out.println();
+		}
+		
+	}
 }
